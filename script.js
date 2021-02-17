4a5e709860c6fd143d2782526d2f6279095d9797
LauraJin8 <Laura.jin@ocsbstudent.ca>
2021-02-17 15:23:45 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let height = 0
+let baseOne = 0
+let baseTwo = 0
+
+document.getElementById('calculate').addEventListener('click', buttonOne)
+
+function buttonOne() {
+  height = document.getElementById('height').value
+  height = parseInt(height)
+  baseOne = document.getElementById('base-one').value
+  baseOne = parseInt(baseOne)
+  baseTwo = document.getElementById('base-two').value
+  baseTwo = parseInt(baseTwo)
+  number = (baseOne + baseTwo)/2 * height
+  document.getElementById('area').innerHTML = (number)
+}
