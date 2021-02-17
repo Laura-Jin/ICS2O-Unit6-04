b84237d47aeacb67b2e679597cb11048bcaf4da8
LauraJin8 <Laura.jin@ocsbstudent.ca>
2021-02-17 15:31:17 +0000

    defined variable number and added spaces

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 let height = 0
 let baseOne = 0
 let baseTwo = 0
+let number = 0
 
 document.getElementById('calculate').addEventListener('click', buttonOne)
 
-function buttonOne() {
+function buttonOne () {
   height = document.getElementById('height').value
   height = parseInt(height)
   baseOne = document.getElementById('base-one').value
   baseOne = parseInt(baseOne)
   baseTwo = document.getElementById('base-two').value
   baseTwo = parseInt(baseTwo)
-  number = (baseOne + baseTwo)/2 * height
+  number = (baseOne + baseTwo) / 2 * height
   document.getElementById('area').innerHTML = (number)
 }
